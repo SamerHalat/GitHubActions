b7ed73ea32af27961259ed65c3c97abfbd29ebc5
Pablofsfs <pabupablo2000@gmail.com>
2025-05-24 10:31:22 -0300

    forcando erro

--- test/example.test.js
+++ test/example.test.js
@@ -1,6 +1,6 @@
 const assert = require('assert')
     describe('Exemplo de teste', function(){
         it('deve somar dois numeros corretamente', function(){
-             assert.strictEqual(1+2, 2)
+             assert.strictEqual(1+2, 3)
                         });
     });
